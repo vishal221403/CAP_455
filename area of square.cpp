af825039b403a359d12602afd76cc8abcef5450e
vishal221403 <154316500+vishal221403@users.noreply.github.com>
2024-08-25 19:06:59 +0530

    Add files via upload

---
+++ area of square.cpp
@@ -0,0 +1,22 @@
+#include<conio.h>
+#include<iostream>
+#include<stdio.h>
+#include<dos.h>
+using namespace std;
+
+int main()
+{
+int side,area ;
+
+cout<<"enter number :";
+cin>>side ;
+
+
+area=side *side;
+
+cout<<"area of square is : "<<area;
+
+return 0;
+
+
+}
