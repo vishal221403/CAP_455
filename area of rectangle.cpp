3472df85c153a15469d4ef3dfbf85b6ec1c5c190
vishal221403 <154316500+vishal221403@users.noreply.github.com>
2024-08-25 19:06:24 +0530

    Add files via upload

---
+++ area of rectangle.cpp
@@ -0,0 +1,23 @@
+#include<conio.h>
+#include<iostream>
+#include<stdio.h>
+#include<dos.h>
+using namespace std;
+
+int main ()
+{
+int length,breadth,area;
+cout<<"enter length : ";
+cin>>length;
+
+cout<<"enter breadth :";
+cin>>breadth;
+
+area=2*(length+breadth);
+
+cout<<"area of rectangle is :"<<area;
+
+
+
+return 0;
+}
